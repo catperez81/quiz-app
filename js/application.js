86a442ff6150c5bfbfac449ce8f247cbc7700309
catperez81 <cat@healthsherpa.com>
2015-11-04 17:07:10 -0800

    all files added

---
+++ js/application.js
@@ -0,0 +1,16 @@
+
+// $(document).ready(function() {
+//   alert('Oh, hi there, world');
+// });
+
+
+$(document).ready(function() {
+  
+});
+
+
+
+// user clicks choice and advances to next question
+// log but don't show choice
+// logic for choices to determine city
+// user can start a new game which clears all logged answers
